b8b07be747a1d3ec6f111d79682cc4d41f5026db
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-02 14:44:56 +0300

    Test structure

---
+++ src/test/java/l/rodomanova/SimpleTest.java
@@ -0,0 +1,44 @@
+package l.rodomanova;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openDemoqaPage() {
+        System.out.println("### @BeforeEach");
+        Selenide.open("https://demoqa.com/automation-practice-form");
+    }
+
+    @AfterEach
+    void close() {
+        System.out.println("### @AfterEach");
+        WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest() {
+        System.out.println("###     @Test 0");
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###     @Test 1");
+    }
+
+}
+
