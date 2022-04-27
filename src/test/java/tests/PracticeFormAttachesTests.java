709e34f14df8898c8dd00270ed339a7627cd0792
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-27 21:02:18 +0300

    Tag

--- src/test/java/tests/PracticeFormAttachesTests.java
+++ src/test/java/tests/PracticeFormAttachesTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("demoqa")
 public class PracticeFormAttachesTests extends TestBase {
 
 
