7ae07900732fc1ad5b43e26c6e10a2cfd814f7b7
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-27 21:51:35 +0300

    Correct2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,7 +15,7 @@ public class TestBase {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
